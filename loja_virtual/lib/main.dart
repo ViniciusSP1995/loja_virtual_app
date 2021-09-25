de6ed380671146664bf06d4d5abd6ae1c676fda3
ViniciusSP1995 <vds1995sp@gmail.com>
2021-09-25 19:37:59 -0300

    composicao loja virtual - drawer, tiles, and screens

---
+++ loja_virtual/lib/main.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+import 'package:loja_virtual/screens/home_screen.dart';
+import 'package:firebase_core/firebase_core.dart';
+
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
+
+  runApp(new MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: "Flutter's Clothing",
+      theme: ThemeData(primarySwatch: Colors.blue, primaryColor: Color.fromARGB(255, 4, 125, 141)),
+      debugShowCheckedModeBanner: false,
+      home: HomeScreen(),
+    );
+  }
+}
