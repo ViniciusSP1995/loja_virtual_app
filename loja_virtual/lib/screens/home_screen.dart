8be296b02d5ca7b32677fa59a91ed6712c1370ea
ViniciusSP1995 <vds1995sp@gmail.com>
2021-09-29 17:51:08 -0300

    orders screens, orders tab and order tile

--- loja_virtual/lib/screens/home_screen.dart
+++ loja_virtual/lib/screens/home_screen.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:loja_virtual/tabs/home_tab.dart';
+import 'package:loja_virtual/tabs/orders_tab.dart';
 import 'package:loja_virtual/tabs/products_tab.dart';
 import 'package:loja_virtual/widgets/cart_button.dart';
 import 'package:loja_virtual/widgets/custom_drawer.dart';
@@ -30,7 +31,14 @@ class HomeScreen extends StatelessWidget {
           
         ),
         Container(color: Colors.yellow),
-        Container(color: Colors.green),
+        Scaffold(
+          appBar: AppBar(
+            title: Text("Meus Pedidos"),
+            centerTitle: true,
+          ),
+          body: OrdersTab(),
+          drawer: CustomDrawer(_pageController), 
+        )
       ],
     );
   }
--- loja_virtual/lib/widgets/cart_price.dart
+++ loja_virtual/lib/widgets/cart_price.dart
@@ -16,6 +16,10 @@ CartPrice(this.buy);
         padding: EdgeInsets.all(10.0),
         child: ScopedModelDescendant<CartModel>(
           builder: (context, child, model) {
+
+            double price = model.getProductsPrice();
+            double discount = model.getDiscount();
+            double ship = model.getShipPrice();
             return Column(
               crossAxisAlignment: CrossAxisAlignment.stretch,
               children: <Widget> [
@@ -29,31 +33,32 @@ CartPrice(this.buy);
                   mainAxisAlignment: MainAxisAlignment.spaceBetween,
                   children: <Widget> [
                     Text("Subtotal"),
-                    Text("R\$ 0.00")
+                    Text("R\$ ${price.toStringAsFixed(2)}")
                   ]
                 ),
                 Divider(),
                 Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceBetween, children: <Widget>[Text("Desconto"), Text("R\$ 0.00")
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween, children: <Widget>[Text("Desconto"), Text("R\$ - ${discount.toStringAsFixed(2)}")
                   ]
                   ),
                 Divider(),
                   Row(
-                mainAxisAlignment: MainAxisAlignment.spaceBetween, children: <Widget>[Text("Entrega"), Text("R\$ 0.00")
+                mainAxisAlignment: MainAxisAlignment.spaceBetween, children: <Widget>[Text("Entrega"), Text("R\$ ${ship.toStringAsFixed(2)}")
                 ]
                 ),
               Divider(),
               SizedBox(height: 12.0),
-                  Row(mainAxisAlignment: MainAxisAlignment.spaceBetween, children: <Widget>[Text("Total"), Text("R\$ 0.00",
+                  Row(mainAxisAlignment: MainAxisAlignment.spaceBetween, children: <Widget>[Text("Total"), Text("R\$ ${(price + ship - discount).toStringAsFixed(2)}",
                   style: TextStyle(color: Theme.of(context).primaryColor, fontSize: 16.0),
                   )
                   ]
                   ),
               SizedBox(height: 12.0),
               RaisedButton(
-                child: Text("Finaliar Pedido"),
+                child: Text("Finalizar Pedido"),
                 textColor: Colors.white,
                 color: Theme.of(context).primaryColor,
+                onPressed: buy,
               )
               ]
             );
